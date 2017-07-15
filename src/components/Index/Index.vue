a9a5c15a481ce6f3a00257adda8c31fe4ca686c3
Choo <641474670@qq.com>
2017-07-15 22:52:15 +0800

    完成首页组件轮播图

--- src/components/Index/Index.vue
+++ src/components/Index/Index.vue
@@ -5,6 +5,23 @@
 			<i class="fa fa-map-marker" aria-hidden="true"></i>
 			<span class="pos" v-text=" posVal "></span>
 		</header>
+    <div class="demo">
+      <swiper :options="swiperOption">
+        <swiper-slide>
+          <img data-src="../../static/img/twentyheart2.0.jpg" alt="" class="img swiper-lazy">
+          <div class="swiper-lazy-preloader"></div>
+        </swiper-slide>
+        <swiper-slide>
+          <img data-src="../../static/img/twentyheart.jpg" alt="" class="img swiper-lazy">
+          <div class="swiper-lazy-preloader"></div>
+        </swiper-slide>
+        <swiper-slide>
+          <img data-src="../../static/img/twentyheart2.0.jpg" alt="" class="img swiper-lazy">
+          <div class="swiper-lazy-preloader"></div>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
 	</div>
 </template>
 
@@ -13,7 +30,14 @@
 		name: 'index',
 		data() {
 			return {
-				posVal: '广东轻工职业技术学院'
+				posVal: '广东轻工职业技术学院(广州校区)',
+        swiperOption: {
+          pagination: '.swiper-pagination', // 索引圆       
+          autoplay: 3500, // 自动博fag
+          effect: 'slider', // cube: 正方体, fade: 渐隐
+          resistanceRatio: 0, // 值越小抵抗越大越难将slide拖离边缘，0时完全无法拖离。
+          lazyLoading : true // 懒加载
+        }
 			}
 		}
 	}
@@ -22,12 +46,22 @@
 <style scoped lang="scss">
 	header {
 		padding: 10px 20px;
-		background-color: #ccc;
+    border-bottom: 1px solid black;
 		.fa {
 			color: #a6b2da;
 		}
 		.pos {
-
+      color: #ccc;
+      margin-left: 5px;
 		}
 	}
+  .img {
+    width: 100%;
+  }
+  .swiper-lazy-preloader {
+    margin-top: 50px;
+  }
+  .demo {
+    background-color: #f3f1f1;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App';
 import router from './router';
 import VueAxios from 'vue-axios';
 import Axios from 'axios';
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+// 使用swiper轮播组件
+Vue.component('swiper', swiper);
+Vue.component('swiperSlide', swiperSlide);
 
 // 在创建实例的时候默认配置项
 Axios.create({

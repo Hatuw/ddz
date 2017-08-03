eb6e787f3cddb77cc03928dbe6adebae53540ab2
BYChoo <641474670@qq.com>
2017-08-03 23:13:47 +0800

    更改注册接口

--- src/api/register.js
+++ src/api/register.js
@@ -1,28 +1,26 @@
 import fetch from 'utils/fetch';
 
 // 获取验证码请求
-export function getCode (val) {
-	const data = {
-		phone: val,
-		type: 'send'
-	}
-	return fetch({
-		url: '/api/get_code',
-		method: 'post',
-		data
-	})
+export function getCode(val) {
+  const data = new FormData();
+  data.append('phone', val);
+  data.append('type', 'send');
+  return fetch({
+    url: '/api/get_code',
+    method: 'post',
+    data
+  })
 }
 
 // 检查验证码请求
-export function checkCode(vcode,phone) {
-	const data = {
-		vcode,
-		type:'verification',
-		phone
-	}
-	return fetch({
-		url: '/api/check_code',
-		method: 'post',
-		data
-	})
+export function checkCode(vcode, phone) {
+  const data = new FormData();
+  data.append('vcode', vcode);
+  data.append('type', 'verification');
+  data.append('phone', phone);
+  return fetch({
+    url: '/api/check_code',
+    method: 'post',
+    data
+  })
 }
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -3,7 +3,8 @@ import Axios from 'axios';
 // 创建axios实例
 const service = Axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000                  // 请求超时时间
+  timeout: 5000,                  // 请求超时时间
+  headers:{'Content-Type':'application/x-www-form-urlencoded'}
 });
 
 export default service;

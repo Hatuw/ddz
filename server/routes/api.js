6c5e18d59bb5c971586bfb83150f8fe099cd478b
bychoo <641474670@qq.com>
2017-08-25 14:04:23 +0800

    修改es5写法

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,37 +2,37 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var sha1 = require('sha1');
-var bodyParser = require('body-parser');  
- 
-var jsonParser = bodyParser.json()  
-  
-var urlencodedParser = bodyParser.urlencoded({ extended: false }) 
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // 获取微信公众号ticket
 router.post('/get_ticket', (req, res) => {
   var noncestr = 'duodongzhen';
-  var timestamp = + new Date();
+  var timestamp = +new Date();
   var url = req.body.url;
   var appId = 'wx374486a038696729';
   var appScrect = 'a0b12c42569f805a443c23e1f659ef6b';
 
   // js-sdk配置信息获取
-  request.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+ appId +'&secret='+ appScrect +'', (err, respone, body) => {
+  request.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appScrect + '', function(err, respone, body) {
 
     var accessToken = JSON.parse(body).access_token;
 
-    request.get('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+ accessToken +'&type=jsapi', function(err, respone, body) {
+    request.get('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + accessToken + '&type=jsapi', function(err, respone, body) {
 
-      var str = 'jsapi_ticket='+ JSON.parse(body).ticket +'&noncestr='+ noncestr +'&timestamp='+ timestamp +'&url='+ url +'';
+      var str = 'jsapi_ticket=' + JSON.parse(body).ticket + '&noncestr=' + noncestr + '&timestamp=' + timestamp + '&url=' + url + '';
 
       var signature = sha1(str);
-      
+
       res.json({
-        signature,
-        appId,
+        signature: signature,
+        appId: appId,
         ticket: JSON.parse(body).ticket,
-        timestamp,
-        noncestr
+        timestamp: timestamp,
+        noncestr: noncestr
       })
     })
   })
@@ -83,12 +83,12 @@ router.get('/order', (req, res) => {
   }
 });
 
-router.post('/order',urlencodedParser, (req, res) => {
+router.post('/order', urlencodedParser, (req, res) => {
   console.log(req.body);
   res.end();
 })
 
-router.post('/demo',urlencodedParser, (req, res) => {
+router.post('/demo', urlencodedParser, (req, res) => {
   console.log(req.body);
   res.end();
 })
